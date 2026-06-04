a553314cdc9c65b3c5eb1a8f83e1ab2e2b6dad8d
Makar <kolesnykmakar0@gmail.com>
2026-06-04 04:19:44 +0300

    request

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.end("hi from express");
+app.get("/products/:id", (req, res) => {
+  const request = `
+  method: ${req.method},
+  path ${req.path},
+  query ${JSON.stringify(req.query)},
+  url: ${req.url}, 
+  hostmname: ${req.hostname},
+  params: ${JSON.stringify(req.params)},
+  key: ${req.params.id},
+  ip: ${req.ip}`;
+  res.end(request);
 });
 
-app.get("/about", (req, res) => {
-  res.end("page about");
-});
-
-app.get('/contact', (req,res)=>[
-    res.end('contact page')
-])
-
-app.use((req, res) => {
-  res.end("page 404");
+app.get("/users/:id", (req, res) => {
+  const request = `
+  url: ${req.url}, 
+  key: ${req.params.id}`;
+  res.end(request);
 });
 
 module.exports = app;
